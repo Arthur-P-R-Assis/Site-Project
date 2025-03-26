3bfed245b33b3dd0eebf311a8bbdd86d41a6d2ec
Arthur <133927449+Arthur-P-R-Assis@users.noreply.github.com>
2025-03-26 15:07:30 -0300

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,69 @@
 <script>
-
+  export default {
+    data() {
+      return {
+        menuAberto: false
+      };
+    },
+    methods: {
+      toggleMenu() {
+        this.menuAberto = !this.menuAberto;
+      }
+    }
+  };
 </script>
 
 <template>
-    <div class="container">
-      <header>
-        <div class="header-esquerdo">
-          <button class="botao-lista">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-  </svg>
-          </button>
+  <div class="container">
+    <header>
+      <div class="header-esquerdo">
+        <button class="botao-lista" @click="toggleMenu">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+          </svg>
+        </button>
         <h1 class="titulo">LARISTORE</h1>
-        </div>
+      </div>
 
-        <div class="botoes">
-          <button class="botao-carrinho">
-            <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-  </svg>
-          </button>
-          <button class="botao-perfil">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-  </svg>
-          </button>
-        </div>
-      </header>
-      <main>
-
-      </main>
-      <footer>
-        <h4 class="telefone">Telefone: 27998416531</h4>
-        <div class="endereco">
-          <h5>Rua Desembargador Homero Mafra, 60 - Enseada do Suá</h5>
-          <h5>Vitória - ES, 29050-275</h5>
-        </div>
-      </footer>
+      <div class="botoes">
+        <button class="botao-carrinho">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+          </svg>
+        </button>
+        <button class="botao-perfil">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+          </svg>
+        </button>
+      </div>
+    </header>
+    
+    <div v-if="menuAberto" class="menu">
+      <ul>
+        <li>Roupas Masculinas</li>
+        <li>Roupas Femininas</li>
+        <li>Tênis</li>
+        <li>Acessórios</li>
+        <li>Promoções</li>
+      </ul>
     </div>
+
+    <main>
+    </main>
+
+    <footer>
+      <h4 class="telefone">Telefone: 27998416531</h4>
+      <div class="endereco">
+        <h5>Rua Desembargador Homero Mafra, 60 - Enseada do Suá</h5>
+        <h5>Vitória - ES, 29050-275</h5>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <style>
-
-  footer{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 0px;
-    margin: 0;
-    border-top: 4px solid #ccc;
-  }
-
-  footer h5,
-  footer h6,
-  footer h4 {
-    margin: 2px 0;
-    padding: 0;
-    font-weight: bold;
-    text-align: center;
-  }
-
-  .endereco{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 2px;
-    margin-bottom: 10px;
-  }
 
   body {
     margin: 0px;
@@ -78,7 +74,7 @@
     display:flex;
     flex-direction: column;
     justify-content: space-between;
-    background-color: white;
+    background-color: whitesmoke;
     width: 100%;
     height: 100vh;
     margin:0px;
@@ -163,5 +159,69 @@
     height: 25px;
     fill:white;
   }
+
+    footer{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px;
+    margin: 0;
+    border-top: 4px solid #ccc;
+  }
+
+  footer h5,
+  footer h6,
+  footer h4 {
+    margin: 2px 0;
+    padding: 0;
+    font-weight: bold;
+    text-align: center;
+  }
+
+  .endereco{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2px;
+    margin-bottom: 10px;
+  }
+
+  .menu {
+    width: 100%;
+    background-color: black;
+    color: white;
+    position: absolute;
+    top: 80px; 
+    left: 0;
+    height: 100%;
+    width: 30%; 
+    display: flex;
+    justify-content: top;
+    align-items: center;
+    flex-direction: column; 
+  }
+
+  .menu ul {
+    list-style-type: none;
+    padding: 0;
+    width: 100%;
+    display: flex;
+    flex-direction: column; /* Alinha os itens na vertical */
+    align-items: center;
+  }
+
+  .menu ul li {
+    padding: 20px;
+    text-align: center;
+    width: 100%;
+    border-bottom: 1px solid white; /* Para separação entre os itens */
+  }
+
+
+  .menu ul li:hover {
+    background-color: #444;
+    cursor: pointer;
+  }
+
 
 </style>
